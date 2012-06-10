6c9ce7054d8622ba4d0d40c9dd016be5196002b8
Cory Radel <cory.radel@gmail.com>
2012-06-10 13:30:42 +0200

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'd3882c6097123fbb6b98c179f43115775c523297f57b817c58f9802f7d5cdcf1c7331675e186ecbc55d12f242c2cd416a8e72f5cae9b56ed7c60a9f0ea81af2d'
+DemoApp::Application.config.secret_token = '1a5d8652b8a4df82a86e159d20c4fd6c64b352c86fa0c95b806f3c29cc7ca69271e0e2e5b1c6b9c71bba1402c0a70de30a1b96afc74962193cdbc21adeb75030'
